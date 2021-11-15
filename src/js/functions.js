e692076c7d1a0d022638b6ba08ea13a6b6d3de5c
Oscar <oscarindealba@gmail.com>
2021-11-15 14:37:19 -0600

    Aplicacion con bootstrap instalado

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,10 +1,13 @@
 import '../css/components.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import logoweb from '../assets/img/logo-gfx.png';
 
 export const saludar = (nombre) => {
     console.log('Creando etiqueta h1');
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre} eres  Omar`;
+    h1.innerText = `Holas, ${nombre} eres  Omar`;
     document.body.append(h1);
 
 
